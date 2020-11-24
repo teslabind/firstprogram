5df83ebd58928b154123afe83cc2122ab97c02dd
谢天 <xietian@MacBook-Pro.local>
2020-11-24 22:10:48 +0900

    hello world

---
+++ xiebin/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  xiebin
+//
+//  Created by 谢天 on 2020/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ xiebin/xiebinApp.swift
@@ -0,0 +1,17 @@
+//
+//  xiebinApp.swift
+//  xiebin
+//
+//  Created by 谢天 on 2020/11/24.
+//
+
+import SwiftUI
+
+@main
+struct xiebinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
